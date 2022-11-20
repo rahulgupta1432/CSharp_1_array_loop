3e5692271672606739aafbd794507325be4ad30c
rahulgupta1432 <101794784+rahulgupta1432@users.noreply.github.com>
2022-11-21 02:16:08 +0530

    Add files via upload

---
+++ array_loop.cs
@@ -0,0 +1,11 @@
+using System;
+class HelloWorld {
+  static void Main() {
+   int[] numbers = { 4, 5, 6, 1, 2, 3, -2, -1, 0 };
+foreach (int i in numbers)
+{
+    System.Console.Write(" {0} ", i);
+}
+// Output: 4 5 6 1 2 3 -2 -1 0
+  }
+}
